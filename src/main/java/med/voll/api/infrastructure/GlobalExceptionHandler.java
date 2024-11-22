251e9b20498756858102fe31ceb2a8a9805d3d07
TrujiDev <eetgrisales@gmail.com>
2024-11-21 20:56:57 -0500

    feat: add global exception handler
    - Implemented a global exception handler using `@RestControllerAdvice` to handle common exceptions:
      - `EntityNotFoundException`: returns a 404 response.
      - `MethodArgumentNotValidException`: returns a 400 response with validation error details.
    - Added a helper record `ErrorValidationData` to structure validation error messages.
    
    This improves error handling across the API, providing consistent responses for common exceptions.

---
+++ src/main/java/med/voll/api/infrastructure/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package med.voll.api.infrastructure;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+  @ExceptionHandler(EntityNotFoundException.class)
+  public ResponseEntity handleEntityNotFound() {
+    return ResponseEntity.notFound().build();
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity handleMethodArgumentNotValid(MethodArgumentNotValidException e) {
+    var errors = e.getFieldErrors().stream().map(ErrorValidationData::new).toList();
+    return ResponseEntity.badRequest().body(errors);
+  }
+
+  private record ErrorValidationData(String field, String msg) {
+    public ErrorValidationData(FieldError error) {
+      this(error.getField(), error.getDefaultMessage());
+    }
+  }
+
+}
