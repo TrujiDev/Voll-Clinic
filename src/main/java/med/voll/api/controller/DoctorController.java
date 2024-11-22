a314764c7ef6e2208804b19561f6bee17c475caa
TrujiDev <eetgrisales@gmail.com>
2024-11-21 20:52:30 -0500

    refactor: enhance DoctorController with ResponseEntity and additional endpoint
    - Updated all methods to use `ResponseEntity` for better HTTP response handling.
    - Added URI creation in the `registerDoctor` method using `UriComponentsBuilder` to provide location headers for newly created resources.
    - Introduced a new endpoint (`GET /doctors/{id}`) to retrieve a specific doctor by ID, returning a `ResponseDoctorDTO`.
    - Replaced direct DTO handling with `ResponseDoctorDTO` for consistent API responses.
    - Improved readability and maintainability of controller methods.
    
    These changes enhance the REST API's compliance with HTTP standards and provide a more robust response structure.

--- src/main/java/med/voll/api/controller/DoctorController.java
+++ src/main/java/med/voll/api/controller/DoctorController.java
@@ -2,12 +2,16 @@ package med.voll.api.controller;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
-import med.voll.api.doctor.*;
+import med.voll.api.domain.doctor.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("/doctors")
@@ -17,28 +21,40 @@ public class DoctorController {
   private DoctorRepository doctorRepository;
 
   @PostMapping
-  public void registerDoctor(@RequestBody @Valid CreateDoctorDTO createDoctorDTO) {
-    doctorRepository.save(new Doctor(createDoctorDTO));
+  public ResponseEntity<ResponseDoctorDTO> registerDoctor(@RequestBody @Valid CreateDoctorDTO createDoctorDTO, UriComponentsBuilder uriComponentsBuilder) {
+    Doctor doctor = doctorRepository.save(new Doctor(createDoctorDTO));
+    ResponseDoctorDTO responseDoctorDTO = ResponseDoctorDTO.from(doctor);
+    URI url = uriComponentsBuilder.path("/doctors/{id}").buildAndExpand(doctor.getId()).toUri();
+    return ResponseEntity.created(url).body(responseDoctorDTO);
   }
 
   @GetMapping
-  public Page<ReadDoctorsDTO> listDoctors(@PageableDefault(size = 4) Pageable pagination) {
-    return doctorRepository.findByIsActiveTrue(pagination).map(ReadDoctorsDTO::new);
+  public ResponseEntity<Page<ReadDoctorsDTO>> listDoctors(@PageableDefault(size = 4) Pageable pagination) {
+    return ResponseEntity.ok(doctorRepository.findByIsActiveTrue(pagination).map(ReadDoctorsDTO::new));
 //    return doctorRepository.findAll(pagination).map(ReadDoctorsDTO::new);
   }
 
   @PutMapping
   @Transactional
-  public void updateDoctor(@RequestBody @Valid UpdateDoctorDTO updateDoctorDTO) {
+  public ResponseEntity<ResponseDoctorDTO> updateDoctor(@RequestBody @Valid UpdateDoctorDTO updateDoctorDTO) {
     Doctor doctor = doctorRepository.getReferenceById(updateDoctorDTO.id());
     doctor.updateData(updateDoctorDTO);
+    return ResponseEntity.ok(ResponseDoctorDTO.from(doctor));
   }
 
   @DeleteMapping("/{id}")
   @Transactional
-  public void deleteDoctor(@PathVariable Long id) {
+  public ResponseEntity<Void> deleteDoctor(@PathVariable Long id) {
     Doctor doctor = doctorRepository.getReferenceById(id);
     doctor.deactivateDoctor();
+    return ResponseEntity.noContent().build();
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<ResponseDoctorDTO> getDoctorByID(@PathVariable Long id) {
+    Doctor doctor = doctorRepository.getReferenceById(id);
+    var responseDoctorDTO = ResponseDoctorDTO.from(doctor);
+    return ResponseEntity.ok(responseDoctorDTO);
   }
 
 }
