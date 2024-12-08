7491d092a3bb3a74fc10ed982bcaa6b68726cf47
TrujiDev <eetgrisales@gmail.com>
2024-12-08 12:38:06 -0500

    Refactored Spring Security Configuration:
    - Removed unnecessary imports.
    - Disabled CSRF and CORS protection.
    - Configured session management to be stateless.
    - Allowed public access to the /login endpoint.
    - Updated method visibility to be compatible with Spring Security 6.
    - Introduced `@Autowired` annotation for better dependency injection.

--- src/main/java/med/voll/api/infrastructure/security/SecurityConfig.java
+++ src/main/java/med/voll/api/infrastructure/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package med.voll.api.infrastructure.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -7,31 +8,36 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+  @Autowired
+  private SecurityFilter securityFilter;
+
   @Bean
-  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    return http.csrf(csrf -> csrf.disable())
-        .cors(cors -> cors.disable())
+  public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    httpSecurity
+        .csrf(AbstractHttpConfigurer::disable)
         .sessionManagement(sm -> sm.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-        .authorizeHttpRequests(req -> {
-          req.requestMatchers(HttpMethod.POST, "/login").permitAll();
-          req.anyRequest().authenticated();
-        })
-//        .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
-        .build();
+        .authorizeHttpRequests(authorize -> authorize
+            .requestMatchers(HttpMethod.POST, "/login").permitAll()
+            .anyRequest().authenticated()
+        )
+        .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class);
+    return httpSecurity.build();
   }
 
   @Bean
-  public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-    return authenticationConfiguration.getAuthenticationManager();
+  public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+    return configuration.getAuthenticationManager();
   }
 
   @Bean
