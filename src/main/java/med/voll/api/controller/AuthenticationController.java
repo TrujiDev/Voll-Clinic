062e4cd71bd08cc498302b4a1fe502e270ee319e
TrujiDev <eetgrisales@gmail.com>
2024-12-08 12:32:32 -0500

    feat: refactor AuthenticationController for JWT token generation
    - Refactors the `AuthenticationController` to include JWT token generation upon user authentication.
    - Adds a `TokenService` to handle the creation of JWT tokens for authenticated users.
    - The `TokenService` is injected into the controller to generate tokens using user information from the authentication process.
    - Now, upon successful authentication, a JWT token is returned in the response.

--- src/main/java/med/voll/api/controller/AuthenticationController.java
+++ src/main/java/med/voll/api/controller/AuthenticationController.java
@@ -2,6 +2,9 @@ package med.voll.api.controller;
 
 import jakarta.validation.Valid;
 import med.voll.api.domain.users.AuthUserData;
+import med.voll.api.domain.users.User;
+import med.voll.api.infrastructure.security.JwtData;
+import med.voll.api.infrastructure.security.TokenService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -19,11 +22,15 @@ public class AuthenticationController {
   @Autowired
   private AuthenticationManager authenticationManager;
 
+  @Autowired(required = true)
+  private TokenService tokenService;
+
   @PostMapping()
-  public ResponseEntity authenticateUser(@RequestBody @Valid AuthUserData authUserData) {
-    Authentication token = new UsernamePasswordAuthenticationToken(authUserData.login(), authUserData.password());
-    authenticationManager.authenticate(token);
-    return ResponseEntity.ok().build();
+  public ResponseEntity<JwtData> authenticateUser(@RequestBody @Valid AuthUserData authUserData) {
+    Authentication authenticationToken = new UsernamePasswordAuthenticationToken(authUserData.login(), authUserData.password());
+    Authentication authenticatedUser = authenticationManager.authenticate(authenticationToken);
+    String jwtToken = tokenService.generateToken((User) authenticatedUser.getPrincipal());
+    return ResponseEntity.ok(new JwtData(jwtToken));
   }
 
 }
