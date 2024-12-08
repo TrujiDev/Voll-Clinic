2a7d15bb6495ba8ad787564282c30ad2efc8011d
TrujiDev <eetgrisales@gmail.com>
2024-12-08 12:43:16 -0500

    A new controller named `PatientController` has been added to manage patient-related operations. This controller provides endpoints for listing, updating, and deleting patients.
    Features:
    - **Listing Patients**: The `list` endpoint allows retrieving a paginated list of patients. It uses `Pageable` for pagination and sorts patients by their name.
    - **Updating Patients**: The `update` endpoint allows updating a patient's information based on provided `PatientUpdateData`.
    - **Deleting Patients**: The `delete` endpoint allows deactivating a patient by setting the `active` status to `false`.
    
    Details of the change:
    - The `PatientRepository` is autowired and used to interact with the database.
    - `list` method: Returns a paginated list of patients mapped to `ListPatientData`.
    - `update` method: Retrieves a patient by ID and updates its details using the provided `PatientUpdateData`.
    - `delete` method: Retrieves a patient by ID and deactivates it.
    - All transactional methods (`update` and `delete`) ensure data integrity within a transaction.

---
+++ src/main/java/med/voll/api/controller/PatientController.java
@@ -0,0 +1,40 @@
+package med.voll.api.controller;
+
+import jakarta.transaction.Transactional;
+import jakarta.validation.Valid;
+import med.voll.api.domain.patient.ListPatientData;
+import med.voll.api.domain.patient.PatientRepository;
+import med.voll.api.domain.patient.PatientUpdateData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/patients")
+public class PatientController {
+
+  @Autowired
+  private PatientRepository patientRepository;
+
+  @GetMapping
+  public Page<ListPatientData> list(@PageableDefault(page = 0, size = 10, sort = {"name"}) Pageable pageable) {
+    return patientRepository.findAll(pageable).map(ListPatientData::new);
+  }
+
+  @PostMapping
+  @Transactional
+  public void update(@RequestBody @Valid PatientUpdateData patientUpdateData) {
+    var patient = patientRepository.getReferenceById(patientUpdateData.id());
+    patient.update(patientUpdateData);
+  }
+
+  @DeleteMapping("/{id}")
+  @Transactional
+  public void delete(@PathVariable Long id) {
+    var patient = patientRepository.getReferenceById(id);
+    patient.deactivate();
+  }
+
+}
