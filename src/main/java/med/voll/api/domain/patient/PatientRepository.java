b93cc694c2b7ebc0fcf41f21fc9160ccef0e1a94
TrujiDev <eetgrisales@gmail.com>
2024-12-12 14:04:35 -0500

    feat(patient): add method to check if a patient is active
    - Added `findActiveById` method to verify the active status of a patient by ID.
    
    This enhances the ability to validate patient records efficiently.

--- src/main/java/med/voll/api/domain/patient/PatientRepository.java
+++ src/main/java/med/voll/api/domain/patient/PatientRepository.java
@@ -3,4 +3,5 @@ package med.voll.api.domain.patient;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PatientRepository extends JpaRepository<Patient, Long> {
+  boolean findActiveById(Long patientId);
 }
