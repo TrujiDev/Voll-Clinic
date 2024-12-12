13b51e24ff34df5fc6505cb5ce93800a1b0fd9ba
TrujiDev <eetgrisales@gmail.com>
2024-12-12 17:21:01 -0500

    enhancement(repository): modify `findActiveById` query to improve performance
    - Updated the `findActiveById` query in `PatientRepository` to select only the `active` field directly.
    - This change reduces the data returned by the query, improving performance when checking the active status of a patient.
    - The new query is more efficient and straightforward.
    
    Related to performance optimization of patient status checks.

--- src/main/java/med/voll/api/domain/patient/PatientRepository.java
+++ src/main/java/med/voll/api/domain/patient/PatientRepository.java
@@ -1,7 +1,13 @@
 package med.voll.api.domain.patient;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PatientRepository extends JpaRepository<Patient, Long> {
+  @Query("""
+      SELECT p.active
+      FROM Patient p
+      WHERE p.id = :patientId
+      """)
   boolean findActiveById(Long patientId);
 }
