2311c13fa21169ec1023baad9c403d5f1d6d944f
TrujiDev <eetgrisales@gmail.com>
2024-12-12 14:03:55 -0500

    feat(doctor): add query to choose a random available doctor and validate input
    - Added `chooseRandomDoctor` method with a custom query to fetch a random active doctor based on specialty and availability.
    - Included validation annotations (`@NotNull` and `@Future`) for input parameters to ensure valid data.
    - Introduced `findIsActiveById` method to verify if a doctor is active by ID.
    
    These changes enhance the functionality for scheduling and doctor selection.

--- src/main/java/med/voll/api/domain/doctor/DoctorRepository.java
+++ src/main/java/med/voll/api/domain/doctor/DoctorRepository.java
@@ -1,9 +1,31 @@
 package med.voll.api.domain.doctor;
 
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDateTime;
 
 public interface DoctorRepository extends JpaRepository<Doctor, Long> {
   Page<Doctor> findByIsActiveTrue(Pageable pagination);
+
+  @Query("""
+      SELECT d
+      FROM Doctor d
+      WHERE d.isActive = true
+      AND d.specialty = :specialty
+      AND d.id NOT IN (
+        SELECT c.doctor.id
+        FROM Consultation c
+        WHERE c.date = :date
+      )
+      ORDER BY rand()
+      LIMIT 1
+      """)
+  Doctor chooseRandomDoctor(Specialty specialty, @NotNull @Future LocalDateTime date);
+
+  boolean findIsActiveById(Long doctorId);
 }
