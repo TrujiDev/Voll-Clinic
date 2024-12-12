65d8005250a644aeba3e444580a770afd7ceeafd
TrujiDev <eetgrisales@gmail.com>
2024-12-12 17:18:42 -0500

    feat(validations): update DoctorAvailabilityChecker to exclude cancelled consultations
    - Updated `DoctorAvailabilityChecker` to ensure that the availability check only considers active consultations without a cancellation reason.
    - The validation now prevents scheduling consultations with doctors who have existing, active but cancelled consultations at the specified date.
    - Ensures that the scheduling logic accounts for active consultations only.
    
    This change improves the reliability of doctor availability checks by excluding consultations marked as cancelled.

--- src/main/java/med/voll/api/domain/consultation/validations/reservation/DoctorAvailabilityChecker.java
+++ src/main/java/med/voll/api/domain/consultation/validations/reservation/DoctorAvailabilityChecker.java
@@ -1,16 +1,18 @@
-package med.voll.api.domain.consultation.validations;
+package med.voll.api.domain.consultation.validations.reservation;
 
 import med.voll.api.domain.consultation.ConsultationRepository;
 import med.voll.api.domain.consultation.ReservationData;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class DoctorAvailabilityChecker implements InquiriesValidator {
 
+  @Autowired
   private ConsultationRepository consultationRepository;
 
   public void validate(ReservationData data) {
-    var doctorOccupied = consultationRepository.existsByDoctorIdAndDate(data.doctorId(), data.date());
+    var doctorOccupied = consultationRepository.existsByDoctorIdAndDateAndCancelReasonIsNull(data.doctorId(), data.date());
 
     if (doctorOccupied) {
       throw new RuntimeException("Doctor is not available at the specified date");
