c27b3fb4c0f352e258600829fa9f310d3cfc93f8
TrujiDev <eetgrisales@gmail.com>
2024-12-12 14:10:08 -0500

    feat(validation): add ThirtyMinuteAppointmentRule to ensure consultations are scheduled at least 30 minutes in advance
    - Introduced `ThirtyMinuteAppointmentRule` as an `InquiriesValidator` to enforce that consultations must be scheduled at least 30 minutes in advance from the current time.
    - The validation checks the time difference between the current date and the scheduled consultation date.
    - Throws a `RuntimeException` if the consultation is scheduled less than 30 minutes in advance.
    
    This rule ensures patients and doctors have sufficient time to prepare for the consultation.

---
+++ src/main/java/med/voll/api/domain/consultation/validations/ThirtyMinuteAppointmentRule.java
@@ -0,0 +1,22 @@
+package med.voll.api.domain.consultation.validations;
+
+import med.voll.api.domain.consultation.ReservationData;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+@Component
+public class ThirtyMinuteAppointmentRule implements InquiriesValidator {
+
+  public void validate(ReservationData data) {
+    var consultationDate = data.date();
+    var currentDate = LocalDateTime.now();
+    var difference = Duration.between(currentDate, consultationDate).toMinutes();
+
+    if (difference < 30) {
+      throw new RuntimeException("Consultation must be scheduled at least 30 minutes in advance");
+    }
+  }
+
+}
