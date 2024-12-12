3024160d9fcd2aef8b1eddb93645b4db494a8bce
TrujiDev <eetgrisales@gmail.com>
2024-12-12 14:09:29 -0500

    feat(validation): add ScheduleValidation to enforce consultation time rules
    - Created `ScheduleValidation` as a component implementing `InquiriesValidator`.
    - This validator checks if the consultation date falls on a Sunday or outside of standard working hours (7 AM to 6 PM).
    - Throws a `RuntimeException` if the consultation time is not within acceptable working hours.
    
    This validation ensures consultations are scheduled only during standard working hours and not on Sundays.

---
+++ src/main/java/med/voll/api/domain/consultation/validations/ScheduleValidation.java
@@ -0,0 +1,22 @@
+package med.voll.api.domain.consultation.validations;
+
+import med.voll.api.domain.consultation.ReservationData;
+import org.springframework.stereotype.Component;
+
+import java.time.DayOfWeek;
+
+@Component
+public class ScheduleValidation implements  InquiriesValidator {
+
+  public void validate(ReservationData data) {
+    var consultationDate = data.date();
+    var sunday = consultationDate.getDayOfWeek().equals(DayOfWeek.SUNDAY);
+    var openingHour = consultationDate.getHour() < 7;
+    var closingHour = consultationDate.getHour() > 18;
+
+    if (sunday || openingHour || closingHour) {
+      throw new RuntimeException("Time selected after working hours");
+    }
+  }
+
+}
