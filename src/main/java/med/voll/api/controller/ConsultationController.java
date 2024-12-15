bcc9bd2373be7d328299904cdb1f3ad6c2f0a3b9
TrujiDev <eetgrisales@gmail.com>
2024-12-15 10:25:53 -0500

    feat(controller): agregar autenticación JWT a `ConsultationController`
    Se ha implementado la autenticación JWT en el controlador `ConsultationController` para asegurar que solo usuarios autorizados puedan realizar reservaciones o cancelar consultas. Esto se logra mediante la anotación `@SecurityRequirement(name = "bearer-jwt")`.
    
    Cambios realizados:
    - Anotación de Seguridad: Se añadió `@SecurityRequirement(name = "bearer-jwt")` en el nivel del controlador para requerir autenticación basada en JWT para todas las rutas asociadas con reservas y cancelaciones.
    - Método `createReservation`: No se realizaron cambios específicos aquí.
    - Método `cancelReservation`: No se realizaron cambios específicos aquí.
    
    Estos cambios fortalecen la seguridad del sistema y aseguran que solo usuarios autenticados puedan realizar operaciones críticas relacionadas con las consultas.

--- src/main/java/med/voll/api/controller/ConsultationController.java
+++ src/main/java/med/voll/api/controller/ConsultationController.java
@@ -1,5 +1,6 @@
 package med.voll.api.controller;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.api.domain.consultation.CancelReservationData;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/inquiries")
+@SecurityRequirement(name = "bearer-jwt")
 public class ConsultationController {
 
   @Autowired
